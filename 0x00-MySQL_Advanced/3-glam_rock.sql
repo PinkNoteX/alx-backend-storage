68d93e555601bac3ac2c8b8030fe73f4371d7fcf
PinkNoteX <omarfarghaly8@gmail.com>
2024-05-10 01:55:12 +0000

    Task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- list all bands with glam rock as their main style
+
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
