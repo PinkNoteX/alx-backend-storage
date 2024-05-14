cfed0e3b545090a1b46b80cef0458a8fbd4da1dc
PinkNoteX <omarfarghaly8@gmail.com>
2024-05-14 15:14:55 -0700

    Task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" some stats """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_logs = client.logs.nginx
+    # get number of documents in collection
+    docs_n = nginx_logs.count_documents({})
+    get_n = nginx_logs.count_documents({'method': 'GET'})
+    post_n = nginx_logs.count_documents({'method': 'POST'})
+    put_n = nginx_logs.count_documents({'method': 'PUT'})
+    patch_n = nginx_logs.count_documents({'method': 'PATCH'})
+    delete_n = nginx_logs.count_documents({'method': 'DELETE'})
+    get_status = nginx_logs.count_documents({'method': 'GET',
+                                             'path': '/status'})
+    print("{} logs".format(docs_n))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get_n))
+    print("\tmethod POST: {}".format(post_n))
+    print("\tmethod PUT: {}".format(put_n))
+    print("\tmethod PATCH: {}".format(patch_n))
+    print("\tmethod DELETE: {}".format(delete_n))
+    print("{} status check".format(get_status))
